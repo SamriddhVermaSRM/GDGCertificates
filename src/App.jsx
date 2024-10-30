fad63ee991441bd2c761133e505a9a95a2405f09
SamriddhVermaSRM <samriddh_be22it132@srmcem.ac.in>
2024-10-30 19:32:22 +0530

    made it dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,30 @@ import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+	// take the url from user input
+	const url =
+		'https://www.cloudskillsboost.google/public_profiles/b3487dc3-9b6f-4b3a-90bc-8bd65c3a8aea';
 	const [pageData, setPageData] = useState();
 	var data;
 	useEffect(() => {
-		fetchProfilePageString('tii').then((data) => {
+		// fetch full user page data
+		fetchProfilePageString(url).then((data) => {
 			setPageData(data);
 		});
 	}, []);
+	// when page data is fetched, export badge links and everything else
 	pageData ? (data = exportBadgeLinks(pageData)) : console.log('no data');
-	console.log(pageData);
+
+	// received data
+	console.log(data);
+
 	return <></>;
 }
 
 async function fetchProfilePageString(url) {
-	const response = await fetch(
-		'http://localhost:8080/https://www.cloudskillsboost.google/public_profiles/b96d480e-d673-4ae5-9845-06bc9d72c153',
-		{
-			method: 'GET',
-		}
-	);
+	const response = await fetch('http://localhost:8080/' + url, {
+		method: 'GET',
+	});
 	const data = await response.text();
 	return data;
 }
@@ -32,9 +37,9 @@ const exportBadgeLinks = (data) => {
 	const links = data.split('<a class="badge-image" href="').slice(1);
 	const linkToBadge = links.map((link) => link.split('">')[0]);
 	const linkToImage = links.map((link) => link.split('src="')[1].split('"')[0]);
-	console.log(linkToBadge);
-	console.log(linkToImage);
-	console.log(name);
+	// console.log(linkToBadge);
+	// console.log(linkToImage);
+	// console.log(name);
 
 	const obj = {
 		name: name,
